bc5f349a33f85f517d450f55b8619e0252d9ad85
Bisha <bisha.mail.drop@gmail.com>
2024-06-28 21:54:08 +0400

    feat: feed value via command line argument

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    while (1) {
-        char ch;
-
-        scanf("%c", &ch);
-        printf("%d\n", ch);
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < strlen(argv[1]); i++) {
+        printf("%d ", argv[1][i]);
     }
+
+    printf("\n");
+
     return 0;
 }
